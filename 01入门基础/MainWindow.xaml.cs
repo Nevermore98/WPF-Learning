c9906be51180a8a5b9f28396f961a4dc54b6a28c
Nevermore <Nevermorey98@gmail.com>
2024-09-22 23:20:20 +0800

    修复01入门基础 bug

--- 01入门基础/MainWindow.xaml.cs
+++ 01入门基础/MainWindow.xaml.cs
@@ -19,11 +19,12 @@ namespace WPF_01入门基础
         // 因为 ObservableCollection 只在集合变化时（Add、 Remove 等）通知 UI 更新，所以需要初始化一个空集合。
         public ObservableCollection<Color> ColorList { get; set; } = new ObservableCollection<Color>();
 
-        public ObservableCollection<Student> StudentList  = new ObservableCollection<Student>()
+        // 注意只有属性才能绑定，字段不能绑定
+        public ObservableCollection<Student> StudentList { get; set; } = new ObservableCollection<Student>()
         {
-            new Student() { Name = "小明", ClassName = "高一二班" },
-            new Student() { Name = "小王", ClassName = "高二三班" },
-            new Student() { Name = "小张", ClassName = "高一一班" },
+            //new Student() { Name = "小明", ClassName = "高一二班" },
+            //new Student() { Name = "小王", ClassName = "高二三班" },
+            //new Student() { Name = "小张", ClassName = "高一一班" },
         };
 
         public MainWindow()
@@ -35,6 +36,9 @@ namespace WPF_01入门基础
             ColorList.Add(new Color { ColorCode = "Green" });
             ColorList.Add(new Color { ColorCode = "Blue" });
 
+            StudentList.Add(new Student() { Name = "小明", ClassName = "高一二班" });
+            StudentList.Add(new Student() { Name = "小王", ClassName = "高二三班" });
+            StudentList.Add(new Student() { Name = "小张", ClassName = "高一一班" });
 
         }
     }
